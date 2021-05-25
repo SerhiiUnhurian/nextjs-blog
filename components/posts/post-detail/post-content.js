be4a8333cdd2bbfebf2e9b8e6f3ee22e38a8cf95
Serhii Unhurian <unguryan23@gmail.com>
2021-05-25 14:35:54 +0300

    feat: implement rendering images from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
-import ReactMarkDown from 'react-markdown';
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import cls from './post-content.module.css';
@@ -7,10 +8,42 @@ const PostContent = ({ post }) => {
   const { slug, image, title, content } = post;
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  const customComponents = {
+    // img: ({ src, alt }) => (
+    //   <Image
+    //     src={`/images/posts/${slug}/${src}`}
+    //     alt={alt}
+    //     width={600}
+    //     height={300}
+    //   />
+    // ),
+
+    // With the code above we get an error that <div> cannot be a child of <p> // element, to fix this we need to provide customize how <p> is rendered.
+    p: paragraph => {
+      if (paragraph.children[0].type === 'img') {
+        const imageObj = paragraph.children[0];
+        const { src, alt } = imageObj.props;
+
+        return (
+          <div className={cls.image}>
+            <Image
+              src={`/images/posts/${slug}/${src}`}
+              alt={alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={cls.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkDown>{content}</ReactMarkDown>
+      <ReactMarkdown components={customComponents}>{content}</ReactMarkdown>
     </article>
   );
 };
